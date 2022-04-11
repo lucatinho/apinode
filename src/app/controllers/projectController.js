1725c9ed681e3808f0761a9cf864da927bff8eea
Edjunior Lucatte <edjunior.lucatte@fiorilli.com.br>
2022-04-11 09:52:28 -0300

    crud project

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,12 +1,88 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
 
+const Project = require('../models/project');
+const Task = require('../models/task');
+
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/', (req, res) => {
-    res.send({ok: true});
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Project.find().populate(['user', 'tasks']);
+
+        return res.send({projects});
+    } catch (err) {
+        return res.status(400).send({error: 'Erro interno'});
+    }
+});
+
+router.get('/:projectId', async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.projectId).populate(['user', 'tasks']);
+
+        return res.send({project});
+    } catch (err) {
+        return res.status(400).send({error: 'Erro interno'});
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const {title, description, tasks} = req.body;
+
+        const project = await Project.create({title, description, user: req.userId});
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({...task, project: project._id});
+
+            await projectTask.save();
+
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
+
+        return res.send({project});
+    } catch (err) {
+        return res.status(400).send({error: 'Erro interno'});
+    }
+});
+
+router.put('/:projectId', async (req, res) => {
+    try {
+        const {title, description, tasks} = req.body;
+
+        const project = await Project.findByIdAndUpdate(req.params.projectId, {title, description}, {new: true});
+
+        project.tasks = [];
+        await Task.remove({project: project._id})
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({...task, project: project._id});
+
+            await projectTask.save();
+
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
+
+        return res.send({project});
+    } catch (err) {
+        return res.status(400).send({error: 'Erro interno'});
+    }
+});
+
+router.delete('/:projectId', async (req, res) => {
+    try {
+        await Project.findByIdAndRemove(req.params.projectId);
+
+        return res.send({sucess: 'Removido com sucesso'});
+    } catch (err) {
+        return res.status(400).send({error: 'Erro interno'});
+    }
 });
 
 module.exports = app => app.use('/projects', router);
